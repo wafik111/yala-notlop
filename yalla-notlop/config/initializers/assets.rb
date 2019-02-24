25969cea4b8491ad4af7e61263c9e78a130b54bb
monaelshenawey <elshenawey_mona@yahoo.com>
2019-02-24 18:47:21 +0200

    design and registiration

--- yalla-notlop/config/initializers/assets.rb
+++ yalla-notlop/config/initializers/assets.rb
@@ -1,7 +1,22 @@
-# Be sure to restart your server when you modify this file.
+
+
+
+
+ # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile = ['.js', '.css', '*.css.erb']
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( snow.css )
+Rails.application.config.assets.precompile += %w(style.css )
+Rails.application.config.assets.precompile += %w( application.css )
+Rails.application.config.assets.precompile += %w( application.js )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( bootstrap.js )
+Rails.application.config.assets.precompile += %w( 33.jpg )
+Rails.application.config.assets.precompile += %w( 1.png )
+Rails.application.config.assets.precompile += %w( user.jpeg )
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -12,3 +27,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
